17ecee9ba254809f01aa371e5c340e64fbc5320b
Hiranya <hiranyagarbhchoudhary@gmail.com>
2019-12-20 05:17:28 +0530

    connect db, update plots

--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,51 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
 
-# Create your models here.
+
+class Patient(models.Model):
+    aadhar_id = models.BigIntegerField(db_column='AADHAR_ID')
+    patient_id = models.IntegerField(db_column='PATIENT_ID', primary_key=True)
+    name = models.CharField(db_column='NAME', max_length=50)
+    age = models.IntegerField(db_column='AGE')
+    gender = models.CharField(db_column='GENDER', max_length=10)
+    contact = models.CharField(db_column='CONTACT', max_length=15)
+    address = models.CharField(db_column='ADDRESS', max_length=200, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'PATIENT'
+
+
+class Prognosis(models.Model):
+    patient = models.ForeignKey(Patient, models.DO_NOTHING, db_column='PATIENT_ID')
+    appointment_date = models.DateTimeField(db_column='APPOINTMENT_DATE')
+    problem_description = models.CharField(db_column='PROBLEM_DESCRIPTION', max_length=200)
+    number_of_days_suffering = models.IntegerField(db_column='NUMBER_OF_DAYS_SUFFERING')
+    recommended_disease = models.CharField(db_column='RECOMMENDED_DISEASE', max_length=200, blank=True, null=True)
+    bmi = models.FloatField(db_column='BMI', blank=True, null=True)
+    ecg = models.CharField(db_column='ECG', max_length=200, blank=True, null=True)
+    eeg = models.CharField(db_column='EEG', max_length=200, blank=True, null=True)
+    eda = models.CharField(db_column='EDA', max_length=200, blank=True, null=True)
+    lung_sound = models.CharField(db_column='LUNG_SOUND', max_length=200, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'PROGNOSIS'
+
+
+class Treatment(models.Model):
+    patient = models.ForeignKey(Patient, models.DO_NOTHING, db_column='PATIENT_ID')
+    timestamp = models.DateTimeField(db_column='TIMESTAMP')
+    diagnosis = models.CharField(db_column='DIAGNOSIS', max_length=200, blank=True, null=True)
+    prescribed_medicine = models.CharField(db_column='PRESCRIBED_MEDICINE', max_length=200, blank=True, null=True)
+    comments = models.CharField(db_column='COMMENTS', max_length=200, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'TREATMENT'
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Patient
 
 def dashboard_view(reqest, *args, **kwargs):
-    return render(reqest, "dashboard.html", {})
+	patient_det_view = Patient.objects.all()
+	pat_context = {
+		'patient_det_view': patient_det_view,
+	}
+	return render(reqest, "dashboard.html", pat_context)
 
 def pat_history_view(reqest, *args, **kwargs):
     return render(reqest, "pat_history.html", {})
@@ -11,4 +16,4 @@ def appt_online_view(reqest, *args, **kwargs):
     return render(reqest, "appt_online.html", {})
 
 def appt_physical_view(reqest, *args, **kwargs):
-    return render(reqest, "appt_physical.html", {})
+    return render(reqest, "appt_physical.html", {}) 
